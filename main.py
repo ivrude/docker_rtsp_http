cf3d56b6e26c722cfa3202354702d17504d548d4
ivrude <136907387+ivrude@users.noreply.github.com>
2025-03-12 08:55:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -49,4 +49,4 @@ def video_feed():
 if __name__ == '__main__':
     capture_thread = threading.Thread(target=capture_frames)
     capture_thread.start()
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8888)
